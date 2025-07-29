a8abba7f4ecf86c4532d0935bfc944f09c755e1e
2006abhishekabhi@gmail.com <2006abhishekabhi@gmail.com"git config --global user.name "abhishek12444git config --global user.email 2006abhishekabhi@gmail.comgit config --global user.name abhishek12440>
2025-07-29 12:08:34 +0530

    fsign

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fsignup/forgotpassword.dart';
 import 'package:fsignup/login.dart';
+import 'package:fsignup/services.dart';
 import 'package:fsignup/welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false,
       
-      home: Forgotpassword()
+      home: Login()
     );
   }
 }
